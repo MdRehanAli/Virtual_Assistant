f03bbfbde0a3f3f25dc02030096629b9b42e14fc
MdRehanAli <rehanali21d>
2023-06-09 14:53:36 +0600

    Microphone Testing part, using speech_recognition under try and except block

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print('Hello, Dear Assistant!')
-print('lets\'s show your Magic!.......')
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print('Now I am listening')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+except:
+    pass
