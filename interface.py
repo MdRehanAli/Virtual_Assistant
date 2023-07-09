c3f137aecf19f56d27e9becf87d05d77268f8e87
MdRehanAli <rehanali21d>
2023-07-10 01:00:47 +0600

    Assistant photo added

--- interface.py
+++ interface.py
@@ -1,5 +1,15 @@
+#tkinter is a GUI of python
 from tkinter import *
+from PIL import ImageTk,Image
 
 root = Tk()
+
+root.title('Virtual Assistant')
+root.geometry('520x320')
+
+img = ImageTk.PhotoImage(Image.open('Assistant.jpg'))
+panel = Label(root, image=img)
+panel.pack(side='right', fill='both', expand='no')
+
 
 root.mainloop()
