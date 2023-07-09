9064e801ae251a2fb6864898539a793d323ab55a
MdRehanAli <rehanali21d>
2023-07-10 02:25:35 +0600

    Added Text and buttons

--- interface.py
+++ interface.py
@@ -5,11 +5,26 @@ from PIL import ImageTk,Image
 root = Tk()
 
 root.title('Virtual Assistant')
-root.geometry('520x320')
+#root.geometry('520x320')
 
 img = ImageTk.PhotoImage(Image.open('Assistant.jpg'))
 panel = Label(root, image=img)
 panel.pack(side='right', fill='both', expand='no')
 
+userText = StringVar()
+
+userText.set('Your Virtual Assistant')
+userFrame = LabelFrame(root, text='Lena', font=('Railways', 24, 'bold'))
+userFrame.pack(fill='both', expand='yes')
+
+top = Message(userFrame, textvariable=userText, bg='light blue', fg='dark blue')
+top.config(font=("Tahoma", 25, 'bold'))
+top.pack(side='top', fill='both', expand='yes')
+
+btn1 = Button(root, text='Run', font=('Tahoma', 18, 'bold'), bg='light green', fg='Green')
+btn1.pack(fill='x', expand='no')
+
+btn2 = Button(root, text='Close', font=('Tahoma', 18, 'bold'), bg='light yellow', fg='orange', command=root.destroy)
+btn2.pack(fill='x', expand='no')
 
 root.mainloop()
